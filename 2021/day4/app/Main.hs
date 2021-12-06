7112d317fa73122be08867be19aba5e2d1d188fb
Marcus Johnson <mejohnson93@gmail.com>
2021-12-06 11:24:24 -0700

    day4

--- 2021/day4/app/Main.hs
+++ 2021/day4/app/Main.hs
@@ -1,16 +1,30 @@
-import System.IO
+module Main where
+
 import Data.Char
-import Data.List
 import Data.Function
+import Data.List
 import Data.List.Split
+import Part1
+import Part2
+import System.IO
 
 main :: IO ()
 main = do
-  main' "test.txt"
-  main' "input.txt"
+  main' part1 "test.txt"
+  main' part1 "input.txt"
+  main' part2 "test.txt"
+  main' part2 "input.txt"
 
-main' :: String -> IO ()
-main' file = do
+part1' = do
+  main' part1 "test.txt"
+  main' part1 "input.txt"
+
+part2' = do
+  main' part2 "test.txt"
+  main' part2 "input.txt"
+
+main' :: ([String] -> Int) -> String -> IO ()
+main' sln file = do
   print $ "File: " ++ file
   ls <- getLines file
   print . sln $ ls
@@ -20,33 +34,3 @@ getLines file = do
   handle <- openFile file ReadMode
   contents <- hGetContents handle
   return $ lines contents
-
-
-sln :: [String] -> Int
-sln ls =
-  let ls' = splitOn [""] ls
-      nums = head ls'
-      bs = getBoards $ tail ls'
-  in error("not implemented yet")
-
-getBoards :: [[String]] -> [[[Int]]]
-getBoards = map ((map (map read)) . map words) . tail
-
-
-applyNum :: Int -> [[[Int]]] -> [[[Int]]]
-applyNum num bs = map (applyNum' num) bs
-
-applyNum' :: Int -> [[Int]] -> [[Int]]
-applyNum' num b = map (applyNum'' num) b
-
-applyNum'' :: Int -> [Int] -> [Int]
-applyNum'' num row = map (\n -> if n==num then -1; else n) row
-
-boardWins :: [[Int]] -> Bool
-boardWins b = (any rowWin b) || (any rowWin $ transpose b)
-
-rowWin :: [Int] -> Bool
-rowWin = all (-1==)
-
-calcScore :: [[Int]] -> Int
-calcScore b = sum . filter (-1/=) $ concat b
