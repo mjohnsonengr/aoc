acbd24c75cee8e665d3919fe5554589df60d27bd
Marcus Johnson <mejohnson93@gmail.com>
2021-12-07 21:44:48 -0700

    Add stack template file for stack templates.
    Usage: `stack new {name} ~/src/aoc/aoc.hsfiles`

--- 2021/day4/app/Main.hs
+++ 2021/day4/app/Main.hs
@@ -1,31 +1,27 @@
 module Main where
 
-import Data.Char
-import Data.Function
-import Data.List
-import Data.List.Split
 import Part1
 import Part2
 import System.IO
 
 main :: IO ()
 main = do
+  p1
+  p2
+
+p1 = do
+  putStrLn "Part 1"
   main' part1 "test.txt"
   main' part1 "input.txt"
-  main' part2 "test.txt"
-  main' part2 "input.txt"
 
-part1' = do
-  main' part1 "test.txt"
-  main' part1 "input.txt"
-
-part2' = do
+p2 = do
+  putStrLn "Part 2"
   main' part2 "test.txt"
   main' part2 "input.txt"
 
 main' :: ([String] -> Int) -> String -> IO ()
 main' sln file = do
-  print $ "File: " ++ file
+  putStrLn $ "File: " ++ file
   ls <- getLines file
   print . sln $ ls
 
