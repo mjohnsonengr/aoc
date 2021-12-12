1af6fa38e6aecd2a250e235dbd9cfae202a13b70
Marcus Johnson <mejohnson93@gmail.com>
2021-12-12 09:58:34 -0700

    finish day 7

--- 2021/day7/src/Part1.hs
+++ 2021/day7/src/Part1.hs
@@ -1,6 +1,18 @@
-module Part1
-    ( part1
-    ) where
+module Part1 where
+
+import Data.List.Split
+import Data.List
 
 part1 :: [String] -> Int
-part1 ls = 0 -- TODO
+part1 ls = 
+  let crabs = parse ls
+  in minimum $ map (cost crabs) [0..(maximum crabs)]
+
+parse :: [String] -> [Int]
+parse lns = sort . map strToInt . splitOn "," $ head lns
+
+strToInt :: String -> Int 
+strToInt = read
+
+cost :: [Int] -> Int -> Int
+cost crabs to = sum $ map (abs . (to-)) crabs
--- 2021/day7/src/Part2.hs
+++ 2021/day7/src/Part2.hs
@@ -1,6 +1,23 @@
-module Part2
-    ( part2
-    ) where
+module Part2 where
+  
+import Data.List.Split
+import Data.List
 
 part2 :: [String] -> Int
-part2 ls = 0 -- TODO
+part2 ls =
+    let crabs = parse ls
+    in minimum $ map (cost crabs) [0..(maximum crabs)]
+
+parse :: [String] -> [Int]
+parse lns = sort . map strToInt . splitOn "," $ head lns
+
+strToInt :: String -> Int 
+strToInt = read
+
+cost :: [Int] -> Int -> Int
+cost crabs to = sum $ map (fuel to) crabs
+
+fuel :: Int -> Int -> Int
+fuel to from = 
+    let n = abs $ from - to
+    in fromIntegral $  n * (n+1) `div` 2
