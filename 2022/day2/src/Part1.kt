62b91579dfcf145bf72db92b1ecdcbf6011a705d
Marcus Johnson <mejohnson93@gmail.com>
2022-12-01 22:37:14 -0700

    Cleaned up Day 2 (the hindsight burns!!!)

--- 2022/day2/src/Part1.kt
+++ 2022/day2/src/Part1.kt
@@ -2,32 +2,15 @@ import java.io.File
 
 class Part1 {
   fun run(input: String): Int {
-    val points =mapOf(
-      Pair("X", 1),
-      Pair("Y", 2),
-      Pair("Z", 3)
-    )
-    val win = mapOf(
-      Pair("A", "Y"),
-      Pair("B", "Z"),
-      Pair("C", "X")
-    )
-    val tie = mapOf(
-      Pair("A", "X"),
-      Pair("B", "Y"),
-      Pair("C", "Z")
-    )
-    // A = X = rock (score 1)
-    // B = Y = paper (score 2)
-    // C = Z = scissors (score 3)
-    // win = 6 points
     val result = input.lines().map {
-      val plays = Regex("""([A-Z])\s([A-Z])""").find(it)?.groupValues!!
-      val (_, other, my) = plays
-      val winpts = if (win[other]!! == my) 6 else 0
-      val tiepts = if (tie[other]!! == my) 3 else 0
-      val playPts = points[my]!!
-      winpts + playPts + tiepts
+      val other = it[0].code - 'A'.code
+      val my = it[2].code - 'X'.code
+      val winPts =
+        if (my == other) 3
+        else if ((my - 1) % 3 == other) 6
+        else 0
+      val playPts = my % 3 + 1
+      winPts + playPts
     }
     return result.sum()
   }
