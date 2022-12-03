93075fd920bee30d53faf71d89257c054c1b6356
Marcus Johnson <mejohnson93@gmail.com>
2022-12-02 22:20:07 -0700

    fix tests

--- 2022/day3/test/Part1Test.kt
+++ 2022/day3/test/Part1Test.kt
@@ -4,23 +4,8 @@ import kotlin.test.assertEquals
 
 class Part1Test {
   @Test
-  fun sampleSlns() {
-    val samples =
-        listOf(
-            Sample("single line input", 1),
-            Sample("""
-              second input
-              has two lines
-            """, 2))
-
-    for (sample in samples) {
-      assertEquals(sample.result, Part1().run(sample.input))
-    }
-  }
-
-  @Test
   fun input() {
-    println(Part1().run(File("input.txt").readText().trimIndent()))
+    assertEquals(7889, Part1().run(File("input.txt").readText().trimIndent()))
   }
 
   data class Sample(private val rawInput: String, val result: Int) {
--- 2022/day3/test/Part2Test.kt
+++ 2022/day3/test/Part2Test.kt
@@ -3,23 +3,10 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Part2Test {
-  @Test
-  fun sampleSlns() {
-    val samples =
-        listOf(
-            Sample("first input", 1),
-            Sample("""
-              second input
-              has two lines
-            """, 2))
-    for (sample in samples) {
-      assertEquals(sample.result, Part2().run(sample.input))
-    }
-  }
 
   @Test
   fun input() {
-    println(Part2().run(File("input.txt").readText().trimIndent()))
+    assertEquals(2825, Part2().run(File("input.txt").readText().trimIndent()))
   }
 
   data class Sample(private val rawInput: String, val result: Int) {
