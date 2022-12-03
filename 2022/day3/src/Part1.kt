ba29ae0eacd35ca7983e1b662a04a47c752f68d0
Marcus Johnson <mejohnson93@gmail.com>
2022-12-03 16:13:34 -0700

    Apply IDE-suggested fixes.

--- 2022/day3/src/Part1.kt
+++ 2022/day3/src/Part1.kt
@@ -3,26 +3,21 @@ import java.io.File
 class Part1 {
   fun run(input: String): Int {
     val lowercase = ('a'..'z').toList()
-      .withIndex()
-      .map { it.value to it.index + 1 }
-      .toMap()
+      .withIndex().associate { it.value to it.index + 1 }
     val uppercase = ('A'..'Z').toList()
-      .withIndex()
-      .map { it.value to it.index + 27 }
-      .toMap()
+      .withIndex().associate { it.value to it.index + 27 }
     val priorities = lowercase + uppercase
 
-    return input.lines()
-      .map { line ->
-        val line1 = line.substring(0, line.length / 2)
-        val line2 = line.substring(line.length / 2)
-        val common = line1.filter {
-          line2.contains(it)
-        }
-          .toList()
-          .distinct()[0]
-        priorities[common]!!
-    }.sum()
+    return input.lines().sumOf { line ->
+      val line1 = line.substring(0, line.length / 2)
+      val line2 = line.substring(line.length / 2)
+      val common = line1.filter {
+        line2.contains(it)
+      }
+        .toList()
+        .distinct()[0]
+      priorities[common]!!
+    }
   }
 }
 
--- 2022/day3/src/Part2.kt
+++ 2022/day3/src/Part2.kt
@@ -2,29 +2,26 @@ import java.io.File
 
 class Part2 {
   fun run(input: String): Int {
-    val lowercase = ('a'..'z').toList()
-      .withIndex()
-      .map { it.value to it.index + 1 }
-      .toMap()
-    val uppercase = ('A'..'Z').toList()
-      .withIndex()
-      .map { it.value to it.index + 27 }
-      .toMap()
+    val lowercase = ('a'..'z').toList().withIndex().associate { it.value to it.index + 1 }
+    val uppercase = ('A'..'Z').toList().withIndex().associate { it.value to it.index + 27 }
     val priorities = lowercase + uppercase
 
-    return input.lines()
-      .withIndex()
-      .groupBy { it.index / 3 }
-      .map {
-        val (elf1, elf2, elf3) = it.value.map { it.value }
-        val common = elf1.filter {
-          elf2.contains(it) && elf3.contains(it)
+    return input
+        .lines()
+        .withIndex()
+        .groupBy { it.index / 3 }
+        .map { line ->
+          val (elf1, elf2, elf3) =
+            line.value.map { it.value }
+          val common =
+              elf1
+                  .filter { elf2.contains(it) && elf3.contains(it) }
+                  .toList()
+                  .distinct()
+                  .also { require(it.size == 1) }[0]
+          priorities[common]!!
         }
-          .toList()
-          .distinct()
-          .also { require(it.size == 1) }[0]
-        priorities[common]!!
-      }.sum()
+        .sum()
   }
 }
 
